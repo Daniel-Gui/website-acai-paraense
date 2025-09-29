d1da0fe4dc867693d9941a1d64b2894d5246b279
Link686 <brasil.gabriel75@gmail.com>
2025-09-29 20:38:16 -0300

    teste

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<script>
-    let nome = "jonas";
+<script lang="ts">
+    import homepage from '../content/home-page.json' with { type: 'json' };
+    
+    let nome = homepage.title;
 </script>
-<h1>Filho do {nome}</h1> 
+<h1>Filho do {homepage.title}</h1> 
 <p></p>
